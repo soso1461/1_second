34ac2a56fdeb9594d335597e54e08ae5358b9c31
soso1461 <soso1461@naver.com>
2023-03-28 16:27:54 +0900

    123

---
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+        @Controller
+
+public class SampleController {
+    @RequestMapping(value = "/main")
+    public String main(){
+        System.out.println("sssss");
+        return "index.html";
+    }
+}
